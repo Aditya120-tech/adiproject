b29b55e568e2ac7b83951a32869aac96ddc5b857
aditya <adityakanchan200.akak@gmail.com>
2025-03-12 18:58:32 +0530

    First Message

---
+++ src/main/java/com/adiproject/AdiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.adiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdiprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adiproject/AdiprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdiprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
