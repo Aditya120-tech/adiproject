ffc328e0ee722255690c558b17754f1efaff0281
aditya <adityakanchan200.akak@gmail.com>
2025-08-17 14:21:34 +0530

    this is demo

--- src/main/java/com/adiproject/AdiprojectApplication.java
+++ src/main/java/com/adiproject/AdiprojectApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AdiprojectApplication {
-
+	int a =10;
 	public static void main(String[] args) {
 		SpringApplication.run(AdiprojectApplication.class, args);
+
+
 	}
 
 }
